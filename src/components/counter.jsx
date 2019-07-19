4a253ea1d9e2d82e93bca278bc0b4e6fd20ccfba
Syed Kashan Ali <kashanali592@gmail.com>
2019-07-19 17:44:54 +0500

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,54 +1,33 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    // imageUrl: 'https://picsum.photos/200' // random 200x200 image
-    tags: ["tag1", "tag2", "tag3"]
-  };
+    state = {
+        count: 0,
+        tags: ["tag1", "tag2", "tag3"]
+    };
 
-  render() {
-    return (
-      // <img src={this.state.imageUrl} alt=""/>
-      //   style = {{ fontSize:10 }} --> for inline styling direct css
-      /*
-            styles = {
-            fontSize: 10, // will automatically be converted to 10px
-            fontWeight: "bold"
-            };
-            style = {this.styles}} --> for direct inline styling using object
-      */
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-      </React.Fragment>
-    );
-  }
+    // one technique for rendering content conditionally
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
+        return (
+            <ul>
+                {this.state.tags.map(tag => (
+                    <li key={tag}>{tag}</li>
+                ))}
+            </ul>
+        );
+    }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
+    render() {
+        return (
+            // other technique for rendering content conditionally
+            <React.Fragment>
+                {this.state.tags.length === 0 && "Please create a new tag!"}
+                {this.renderTags()}
+            </React.Fragment>
+        );
+    }
 }
 
 export default Counter;
-
-/*
-function Counter() {
-    return (
-        <div>
-      <h1>Hello World</h1>
-      <button>Increment </button>
-      </div>
-      );
-    }
-    
-export default Counter;
-*/
