084da3c82a0283a1c285bd13962ecbcf7abedaff
Syed Kashan Ali <kashanali592@gmail.com>
2019-07-22 13:59:04 +0500

    create stateless functional component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import NavBar from "./components/navbar";
 import Counters from "./components/counters";
 import "./App.css";
 
-// function App() {
-//     return (
-//         <React.Fragment>
-//             <NavBar />
-//             <main className="container">
-//                 <Counters />
-//             </main>
-//         </React.Fragment>
-//     );
-// }
-
-// export default App;
+// This approach is used for defining stateless components
+/* function App() {
+    return (
+        <React.Fragment>
+            <NavBar />
+            <main className="container">
+                <Counters />
+            </main>
+        </React.Fragment>
+    );
+} */
 
 class App extends Component {
     state = {
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <a href="#" className="navbar-brand">
-                    Navbar
-                    <span className="badge badge-pill badge-secondary">
-                        {this.props.totalCounters}
-                    </span>
-                </a>
-            </nav>
-        );
-    }
+// Stateless Functional Component
+
+// for stateless component we pass props in parameter
+const NavBar = (props) => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a href="#" className="navbar-brand">
+                Navbar
+                <span className="badge badge-pill badge-secondary">
+                    {props.totalCounters}
+                </span>
+            </a>
+        </nav>
+    );
 }
 
 export default NavBar;
