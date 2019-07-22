45e8aef0790c31374b47d1e7a22854876ad07772
Syed Kashan Ali <kashanali592@gmail.com>
2019-07-22 10:16:25 +0500

    add parent component and pass data to child component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0
+        value: this.props.value
     };
     handleIncrement = prod => {
         console.log(prod);
         // for reflecting state change on view we use setState method i.e we are setting state again
-        this.setState({ count: this.state.count + 1 });
+        this.setState({ value: this.state.value + 1 });
     };
 
     // one way for passing argument to event other is by direct inline method
@@ -17,7 +17,7 @@ class Counter extends Component {
 
     render() {
         return (
-            <React.Fragment>
+            <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button
                     onClick={() => {
@@ -27,19 +27,19 @@ class Counter extends Component {
                 >
                     Increment
                 </button>
-            </React.Fragment>
+            </div>
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
+        const { value } = this.state;
+        return value === 0 ? "Zero" : value;
     }
 }
 
